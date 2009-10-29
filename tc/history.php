7bc4e00a6f22b901bd8a34efb893d5bd9646af81
tube@pberry.me.uk <tube@pberry.me.uk@f820fc8e-be67-11de-869a-473eb4776397>
2009-10-29 22:42:53 +0000

    Moved league and history from /events to root (see r32).

---
+++ tc/history.php
@@ -0,0 +1,20 @@
+<?php
+	// Events
+	// ------
+	// 129snake
+	// alllines
+	// alphabet
+	// boroughs
+	// bottle
+	// dlr
+	// fullnetwork
+	// mouse
+	// pointsofcompass
+	// random15
+	// roadsstreetslanes
+	// southlondon
+	// tramlink
+	// xmaspark
+	// zone1
+	// zone12
+?>
