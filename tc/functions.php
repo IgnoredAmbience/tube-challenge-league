5aa448100a6225d9ae1b7a4fc7166263ece15c0d
tube@pberry.me.uk <tube@pberry.me.uk@f820fc8e-be67-11de-869a-473eb4776397>
2009-11-06 10:13:42 +0000

    Resolved Issue 19

--- tc/functions.php
+++ tc/functions.php
@@ -4,7 +4,7 @@
 		echo "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n";
 		echo "<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\">\n";
 		echo "\t<head>\n";
-		echo "\t\t<title>Tube Challenge League Tables - UNDER CONSTRUCTION!!</title>\n";
+		echo "\t\t<title>Tube Challenge League Tables</title>\n";
 		echo "\t\t<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />\n";
 		echo "\t\t<link rel=\"stylesheet\" title=\"Default\" type=\"text/css\" href=\"";
 		while($directory_depth)
@@ -16,7 +16,7 @@
 		echo "\t</head>\n";
 		echo "\t<body>\n";
 		echo "\t\t<div id=\"temps\">Page generated at: ".date("H:i").",&nbsp;".date("d.m.y")."&nbsp;[<a class=\"text\" href=\"javascript:location.reload()\">refresh?</a>]&nbsp;</div>\n";
-		echo "\t\t<div id=\"header1\">&nbsp;Tube Challenge League Tables - UNDER CONSTRUCTION!!</div>\n";
+		echo "\t\t<div id=\"header1\">&nbsp;Tube Challenge League Tables</div>\n";
 		echo "\t\t<div id=\"header2\">$page_title&nbsp;</div>\n";
 	}
 	
